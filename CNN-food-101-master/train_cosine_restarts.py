94543ce5e0f377fa8a0bed23ffc82ff2810d0519
Tabami <actharsis@gmail.com>
2021-04-26 23:38:47 +0300

    Update train_cosine_restarts.py

--- CNN-food-101-master/train_cosine_restarts.py
+++ CNN-food-101-master/train_cosine_restarts.py
@@ -58,8 +58,8 @@ def create_dataset(filenames, batch_size):
     .batch(batch_size)\
     .prefetch(tf.data.AUTOTUNE)
 
-initial_learning_rate = 0.0001
-first_decay_steps = 100
+initial_learning_rate = 0.1
+first_decay_steps = 10
 
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
